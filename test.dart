04cf78626c6c3adf8e7d95c803cd6b410f25ab71
Hikky-mah <adejumokehikmat@gmail.com>
2026-06-21 17:24:31 +0100

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,19 @@
+void main() {
+    String? name = "adejumoke";
+    int? age = 23;
+    String? nationality = "nigerian";
+    String? religion = "islam";
+    String? stateOfOrigin = "oyo state";
+    String? address = "oke ero kwara";
+    String? occupation = "government pikin";
+    String? favFood = "eggs";
+    String? favColor = "custard blue";
+    String? hobby = "watching tvd";
+
+    String? paragraph;
+    paragraph = """Hello, my name is $name. I am $age years old. I am a $nationality and I practice $religion.
+     I am from $stateOfOrigin but I live in $address. I am a $occupation. 
+     My fav food is $favFood and my fav color is $favColor. I love $hobby when I am free""";
+
+     print(paragraph);
+}
